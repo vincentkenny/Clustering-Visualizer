87d5721f768729c0db4170249d92bcdf300de340
Kenny Vincent <vincent.kenny98@gmail.com>
2020-09-07 21:09:56 +0700

    centroid animation

--- func.js
+++ func.js
@@ -1,22 +1,21 @@
 class node {
-    constructor(x, y,group) {
+    constructor(x, y, group) {
         this.x = x;
         this.y = y;
-        this.group=group;
+        this.group = group;
     }
 }
 
 var cur_stat;
-var arrNode= new Array();
+var arrNode = new Array();
 var centroid;
-var cycle=0;
-var group_num=0;
+var group_num = 0;
 
 function add(a, b) {
     return a + b;
 }
 
-function set_group(val){
+function set_group(val) {
     group_num = val;
     dropdown = document.getElementsByClassName("dropdown-menu")[0];
     dropdown.style.display = "none";
@@ -25,14 +24,14 @@ function set_group(val){
     btn.innerHTML = "Visualize Clustering";
 }
 
-function view_dropdown(){
+function view_dropdown() {
     dropdown = document.getElementsByClassName("dropdown-menu")[0];
-    if(dropdown.style.display=="block"){
+    if (dropdown.style.display == "block") {
         dropdown.style.display = "none";
-    }else{
+    } else {
         dropdown.style.display = "block";
     }
-    
+
 }
 
 //estrablishing functions and generating grid
@@ -48,10 +47,20 @@ function generate() {
             tag.setAttribute("id", i + "-" + j);
             tag.addEventListener("click", function () {
                 cur_stat = this.id;
+                coords = this.id.split('-');
+                y = coords[0];
+                x = coords[1];
                 if (this.className == "grid") {
                     this.className = "node";
+                    arrNode.push(new node(x, y, 0));
                 } else {
                     this.className = "grid";
+                    for (var k = 0; k < arrNode.length; k++) {
+                        if (arrNode[k].x == x && arrNode[k].y == y) {
+                            arrNode.splice(k, 1);
+                            console.log(x + "-" + y);
+                        }
+                    }
                 }
             });
             tag.addEventListener("mousedown", function () {
@@ -65,13 +74,13 @@ function generate() {
                     if (this.id != cur_stat) {
                         if (this.className == "grid") {
                             this.className = "node";
-                            arrNode.push(new node(x,y,0));
+                            arrNode.push(new node(x, y, 0));
                         } else {
                             this.className = "grid";
-                            for(var k=0;k<arrNode.length;k++){
-                                if(arrNode[k].x == x && arrNode[k].y==y){
-                                    arrNode.splice(k,1);
-                                    console.log(x+"-"+y);
+                            for (var k = 0; k < arrNode.length; k++) {
+                                if (arrNode[k].x == x && arrNode[k].y == y) {
+                                    arrNode.splice(k, 1);
+                                    console.log(x + "-" + y);
                                 }
                             }
                         }
@@ -108,7 +117,7 @@ function scatter_data() {
         for (var i = 0; i < 50; i++) {
             x = Math.floor(Math.random() * 55);
             y = Math.floor(Math.random() * 20);
-            arrNode.push(new node(x, y,0));
+            arrNode.push(new node(x, y, 0));
         }
         arrNode.forEach(function (item) {
             address = item.y + "-" + item.x;
@@ -116,86 +125,107 @@ function scatter_data() {
             data.setAttribute("class", "node");
         });
         centroid = new Array();
-        for(var i=0;i<group_num;i++){
-            do{
-                index = Math.floor(Math.random()*arrNode.length);
-            }while(arrNode[index].group!=0)
-            category = i+1;
-            arrNode[index].group=category;
-            address = arrNode[index].y+"-"+arrNode[index].x;
+        for (var i = 0; i < group_num; i++) {
+            do {
+                index = Math.floor(Math.random() * arrNode.length);
+            } while (arrNode[index].group != 0)
+            category = i + 1;
+            arrNode[index].group = category;
+            address = arrNode[index].y + "-" + arrNode[index].x;
             data = document.getElementById(address);
-            data.setAttribute("class","group"+category);    
-            centroid.push([arrNode[index].x,arrNode[index].y]);
+            data.setAttribute("class", "group" + category);
+            centroid.push([arrNode[index].x, arrNode[index].y]);
         }
-    }else{
+    } else {
         btn = document.getElementById("btn-visualize");
         btn.innerHTML = "Set Number of Group";
     }
 }
-function recalibrate_centroid(group_num,centroid){
-    for(var i=0;i<group_num;i++){
-        cur_category = i+1;
+function recalibrate_centroid(group_num, centroid) {
+    for (var i = 0; i < group_num; i++) {
+        cur_category = i + 1;
         cur_x = 0;
         cur_y = 0;
-        num_member=0;
-        arrNode.forEach(function(item){
-            if(item.group==cur_category){
-                cur_x+=item.x;
-                cur_y+=item.y;
-                num_member+=1;
+        num_member = 0;
+        arrNode.forEach(function (item) {
+            if (item.group == cur_category) {
+                cur_x += item.x;
+                cur_y += item.y;
+                num_member += 1;
             }
         });
-        cur_x/=num_member;
-        cur_y/=num_member;
-        centroid[i] = [cur_x,cur_y];
+        cur_x /= num_member;
+        cur_y /= num_member;
+        cur_x = Math.round(cur_x);
+        cur_y = Math.round(cur_y);
+        centroid[i] = [cur_x, cur_y];
+        address = cur_y + "-" + cur_x;
+        group=i+1;
+        data = document.getElementById(address);
+        data.setAttribute("class","centroid"+group);
+        console.log("centoid "+group+" "+address);
     }
 }
-function reestimate_group(node,centroid){
-    dist=100;
-    group=node.group;
-    for(var i=0;i<centroid.length;i++){
-        centroid_x = centroid[i][0];
-        centroid_y = centroid[i][1];
-        dist_centroid = Math.abs(node.x-centroid_x)+Math.abs(node.y-centroid_y);
-        if(dist_centroid<dist){
-            dist = dist_centroid;
-            group = i+1;
+function reestimate_group(node, centroid) {
+    if (node != null) {
+        dist = 100;
+        group = node.group;
+        for (var i = 0; i < centroid.length; i++) {
+            centroid_x = centroid[i][0];
+            centroid_y = centroid[i][1];
+            dist_centroid = Math.abs(node.x - centroid_x) + Math.abs(node.y - centroid_y);
+            if (dist_centroid < dist) {
+                dist = dist_centroid;
+                group = i + 1;
+            }
         }
-    }
-    node.group = group;
-    address = node.y+"-"+node.x;
-    data = document.getElementById(address);
-    data.setAttribute("class","group"+group);    
-}
-
-function visualize_k_means(){
-    // for(var i=0;i<arrNode.length;i++){
-    //     reestimate_group(arrNode[i],centroid);
-    // }
-    ctr=0
-    myvar = setInterval(function () {
-        reestimate_group(arrNode[ctr],centroid);
-        ctr+=1
-    }, 500);
-    if (ctr == arrNode.length) {
-        clearInterval(myvar);
+        node.group = group;
+        address = node.y + "-" + node.x;
+        data = document.getElementById(address);
+        data.setAttribute("class", "group" + group);        
     }
 }
 
-function testTimer() {
+function visualize_k_means() {
+    console.log(arrNode.length);
+    var ctr = 0;
+    var cycle=0;
+    var myvar = setInterval(function () {
+        console.log(ctr);
+        reestimate_group(arrNode[ctr], centroid);
+        ctr += 1;
+        
+        if(ctr==arrNode.length){
+                clearInterval(myvar);
+                recalibrate_centroid(group_num, centroid);
+            }
+        // if (ctr == arrNode.length && convergent==false) {
+        //     ctr=0;
+        // }
+        // else if(ctr==arrNode.length &&convergent==true){
+        //     clearInterval(myvar);
+        // }
+    }, 50);
 
-    axes = document.getElementsByClassName("node");
-    ctr = 0;
-    myvar = setInterval(function () {
-        axes[ctr].setAttribute("class", "group1");
-        console.log(axes[ctr]);
-    }, 1000);
-    if (ctr == axes.length) {
-        clearInterval(myvar);
-    }
+    
+    console.log(cycle);
+
 }
 
-function stopTimer() {
-    clearInterval(myvar);
-}
+// function testTimer() {
+
+//     axes = document.getElementsByClassName("node");
+//     ctr = 0;
+//     myvar = setInterval(function () {
+//         axes[ctr].setAttribute("class", "group1");
+//         console.log(axes[ctr]);
+//     }, 1000);
+//     if (ctr == axes.length) {
+//         clearInterval(myvar);
+//     }
+// }
+
+// function stopTimer() {
+//     clearInterval(myvar);
+// }
 
